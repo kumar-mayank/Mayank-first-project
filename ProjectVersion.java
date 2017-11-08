939b1da7c333125053deae793f8ca6bf67317c1f
kumar-mayank <33480066+kumar-mayank@users.noreply.github.com>
2017-11-08 14:54:28 +0530

    ProjectVersion.java file to Github

---
+++ ProjectVersion.java
@@ -0,0 +1,6 @@
+public class ProjectVersion {
+   public static void main(String[] args) {
+      // Prints "Project Version" in the terminal window.
+      System.out.println("Project Vesrion: 1");
+   }
+}
